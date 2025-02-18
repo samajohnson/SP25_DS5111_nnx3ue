2b0cfa4da159fac26086618c86d65fc17104662b
samajohnson <sam.anne.johnson@gmail.com>
2025-02-18 01:25:33 +0000

    Updated requirements.txt to include pytest and pylint. Update makefile to include lint and testest to run pylint and pytest checks. Updated normalize_csv based off of feedbackk from pylint to get 10/10.

--- bin/normalize_csv.py
+++ bin/normalize_csv.py
@@ -1,6 +1,17 @@
+"""
+normalize_csv.py
+
+This module normalizes input csvs to be the same format.
+
+Author: Sam Johnosn
+Date: 2000-02-17
+"""
+
+
+
 import sys
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 
 def normalize_csv(input_csv_path: str):
     '''
@@ -16,7 +27,7 @@ def normalize_csv(input_csv_path: str):
     # Ensure file exists
     input_path = Path(input_csv_path)
     assert input_path.exists(), f"File not found: {input_csv_path}"
-    
+
     # Define expected columns mapping
     column_mapping = {
         "Symbol": "symbol",
@@ -24,22 +35,22 @@ def normalize_csv(input_csv_path: str):
         "Change": "price_change",
         "Change %": "price_percent_change"
     }
-    
+
     # Read raw CSV
     df = pd.read_csv(input_csv_path, usecols=column_mapping.keys())
-    
+
     # Rename columns
     df.rename(columns=column_mapping, inplace=True)
-    
+
     # Enforce data types
     df["symbol"] = df["symbol"].astype(str)
     df["price"] = pd.to_numeric(df["price"], errors="coerce")
     df["price_change"] = pd.to_numeric(df["price_change"], errors="coerce")
     df["price_percent_change"] = pd.to_numeric(df["price_percent_change"], errors="coerce")
-    
+
     # Define output path
     output_csv_path = input_path.with_stem(input_path.stem + "_norm")
-    
+
     # Save normalized CSV
     df.to_csv(output_csv_path, index=False)
     print(f"Normalized file saved: {output_csv_path}")
--- makefile
+++ makefile
@@ -14,7 +14,13 @@ ygainers.csv: ygainers.html
 	env/bin/activate && python3 -c "import pandas as pd; raw = pd.read_html('ygainers.html'); raw[0].to_csv('ygainers.csv')"
 
 wjsgainers.html:
-	sudo google-chrome-stable --headless --disable-gpu --dump-dom --no-sandbox --timeout=500 'https://www.wsj.com/market-data/stocks/us/movers' > wjsgainers.html
+	sudo google-chrome-stable --headless --disable-gpu --dump-dom --no-sandbox --timeout=5000 https://www.wsj.com/market-data/stocks/us/movers > wsjgainers.htm
 
 wjsgainers.csv: wjsgainers.html
 	env/bin/activate && python3 -c "import pandas as pd; raw = pd.read_html('wjsgainers.html'); raw[0].to_csv('wjsgainers.csv')"
+
+lint:
+	 env/bin/activate && pylint bin/normalize_csv.py
+
+test: lint
+	 env/bin/activate && pytest -vv tests
