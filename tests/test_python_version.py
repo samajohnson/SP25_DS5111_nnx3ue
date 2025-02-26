477e071952377153a15fa0149f1c2dcfc9a797e6
samajohnson <sam.anne.johnson@gmail.com>
2025-02-26 01:24:51 +0000

    updating python version test to also account for version 3.12

--- tests/test_python_version.py
+++ tests/test_python_version.py
@@ -1,4 +1,4 @@
 import sys
 
 def test_python_version():
-    assert sys.version_info[:2] in [(3, 10), (3, 11)], "Test failed: Python version is not 3.10 or 3.11!"
+    assert sys.version_info[:2] in [(3, 10), (3, 11), (3,12)], "Test failed: Python version is not 3.10, 3.11, or 3.12!"
