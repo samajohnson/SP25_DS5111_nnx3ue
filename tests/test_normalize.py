60b0112ed9f20e02317b9f3c30f27b83d05101c0
samajohnson <sam.anne.johnson@gmail.com>
2025-02-26 02:19:57 +0000

    adding ygainers and updating test normalize

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -6,8 +6,8 @@ sys.path.append('.')
 import bin.normalize_csv as normalize
 
 def test_normalize():
-    #csv_path = Path(__file__).parent / "ygainers.csv"
-    csv_path = '/home/runner/work/SP25_DS5111_nnx3ue/ygainers.csv'
+    csv_path = Path(__file__).parent.parent / "ygainers.csv"
+#    csv_path = '/home/runner/work/SP25_DS5111_nnx3ue/ygainers.csv'
     df = normalize.normalize_csv(str(csv_path))
     #df = normalize.normalize_csv("ygainers.csv")
 
