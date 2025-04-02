40cc0c22bb140c96c30de0c5828af53a86d8a99e
Efrain Olivares <99214069+EfrainOlivaresUVA@users.noreply.github.com>
2025-04-02 01:24:04 -0700

    Update test_python_version.py

--- tests/test_python_version.py
+++ tests/test_python_version.py
@@ -1,4 +1,7 @@
 import sys
 
-def test_python_version():
+def test_python_version():  # Just a head's up that you'll need to update this test when you change validation.yaml
     assert sys.version_info[:2] in [(3, 10), (3, 11), (3,12)], "Test failed: Python version is not 3.10, 3.11, or 3.12!"
+
+
+# not finding the test to check for OS
